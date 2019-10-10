f5a6ba67c726b66709e66cbaa149453d438aa1d5
David Pascual <david.pascualh@estudiante.uam.es>
2019-10-10 11:13:32 +0200

    Detalle en flask a medias

--- P2/public_html/app/routes.py
+++ P2/public_html/app/routes.py
@@ -13,3 +13,9 @@ def index():
     catalogue_data = open(os.path.join(app.root_path,'catalogue/catalogo.json'), encoding="utf-8").read()
     catalogue = json.loads(catalogue_data)
     return render_template('index.html', title = "Home", movies=catalogue['peliculas'])
+
+@app.route('/index/<pelicula_id>')
+def detalle(pelicula_id):
+    catalogue_data = open(os.path.join(app.root_path,'catalogue/catalogo.json'), encoding="utf-8").read()
+    catalogue = json.loads(catalogue_data)
+    return render_template(pelicula_id+'.html', title = 'Detalle', movies=catalogue['peliculas'])

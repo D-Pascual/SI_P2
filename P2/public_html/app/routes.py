92ad121919f18056d61827ac49ff8655df582e0f
David Pascual <david.pascualh@estudiante.uam.es>
2019-10-11 14:32:18 +0200

    Sesion empezado

--- P2/public_html/app/routes.py
+++ P2/public_html/app/routes.py
@@ -14,9 +14,13 @@ def index():
     catalogue = json.loads(catalogue_data)
     return render_template('index.html', title = "Home", movies=catalogue['peliculas'])
 
-@app.route('/index/<titulo>')
+@app.route('/<titulo>')
 def detalle(titulo):
     catalogue_data = open(os.path.join(app.root_path,'catalogue/catalogo.json'), encoding="utf-8").read()
     catalogue = json.loads(catalogue_data)
     movies = catalogue['peliculas']
     return render_template('detail.html', selection=next((item for item in movies if item["titulo"] == titulo), False))
+
+@app.route('/sesion')
+def sesion():
+    return render_template('sesion.html', title = "Sesion")
